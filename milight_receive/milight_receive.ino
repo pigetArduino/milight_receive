d71445caf84706c9e5b6d4543577d6b901b7ac88
maditnerd <remi@madnerd.org>
2016-07-01 18:58:23 +0200

    First autocommit by pigetBot

---
+++ milight_receive/milight_receive.ino
@@ -0,0 +1,153 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <printf.h>
+
+#include "PL1167_nRF24.h"
+#include "MiLightRadio.h"
+
+#define CE_PIN 9
+#define CSN_PIN 10
+
+RF24 radio(CE_PIN, CSN_PIN);
+PL1167_nRF24 prf(radio);
+MiLightRadio mlr(prf);
+
+void setup()
+{
+  Serial.begin(115200);
+  printf_begin();
+  delay(1000);
+  Serial.println("Press a button on your smartphone to receive radiocodes");
+  mlr.begin();
+}
+
+
+static int dupesPrinted = 0;
+static bool receiving = true;
+static bool escaped = false;
+static uint8_t outgoingPacket[7];
+static uint8_t outgoingPacketPos = 0;
+static uint8_t nibble;
+static enum {
+  IDLE,
+  HAVE_NIBBLE,
+  COMPLETE,
+} state;
+
+void loop()
+{
+
+  if (receiving) {
+    if (mlr.available()) {
+      printf("\n");
+      uint8_t packet[7];
+      size_t packet_length = sizeof(packet);
+      mlr.read(packet, packet_length);
+
+      for (int i = 0; i < packet_length; i++) {
+        printf("0x%02X ", packet[i]);
+      }
+    }
+
+    int dupesReceived = mlr.dupesReceived();
+    for (; dupesPrinted < dupesReceived; dupesPrinted++) {
+      printf(".");
+    }
+  }
+
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    uint8_t val = 0;
+    bool have_val = true;
+
+    if (inChar >= '0' && inChar <= '9') {
+      val = inChar - '0';
+    } else if (inChar >= 'a' && inChar <= 'f') {
+      val = inChar - 'a' + 10;
+    } else if (inChar >= 'A' && inChar <= 'F') {
+      val = inChar - 'A' + 10;
+    } else {
+      have_val = false;
+    }
+
+    if (!escaped) {
+      if (have_val) {
+        switch (state) {
+          case IDLE:
+            nibble = val;
+            state = HAVE_NIBBLE;
+            break;
+          case HAVE_NIBBLE:
+            if (outgoingPacketPos < sizeof(outgoingPacket)) {
+              outgoingPacket[outgoingPacketPos++] = (nibble << 4) | (val);
+            } else {
+              Serial.println("# Error: outgoing packet buffer full/packet too long");
+            }
+            if (outgoingPacketPos >= sizeof(outgoingPacket)) {
+              state = COMPLETE;
+            } else {
+              state = IDLE;
+              Serial.println("IDLE");
+            }
+            break;
+          case COMPLETE:
+            Serial.println("# Error: outgoing packet complete. Press enter to send.");
+            state = COMPLETE;
+            break;
+        }
+      } else {
+        switch (inChar) {
+          case ' ':
+          case '\n':
+          case '\r':
+          case '.':
+            if (state == COMPLETE) {
+              mlr.write(outgoingPacket, sizeof(outgoingPacket));
+              Serial.println(outgoingPacket[0]);
+              Serial.println(outgoingPacket[1]);
+              Serial.println(outgoingPacket[2]);
+              Serial.println(outgoingPacket[3]);
+              Serial.println(outgoingPacket[4]);
+              Serial.println(outgoingPacket[5]);
+              Serial.println(outgoingPacket[6]);
+            }
+            if(inChar != ' ') {
+              outgoingPacketPos = 0;
+              state = IDLE;
+              mlr.resend();
+              delay(1);
+            }
+            if (inChar == '.') {
+              mlr.resend();
+              delay(1);
+            }
+            break;
+          case 'x':
+            Serial.println("# Escaped to extended commands: r - Toggle receiver; Press enter to return to normal mode.");
+            escaped = true;
+            break;
+        }
+      }
+    } else {
+      switch (inChar) {
+        case '\n':
+        case '\r':
+          outgoingPacketPos = 0;
+          state = IDLE;
+          escaped = false;
+          break;
+        case 'r':
+          receiving = !receiving;
+          if (receiving) {
+            Serial.println("# Now receiving");
+          } else {
+            Serial.println("# Now not receiving");
+          }
+          break;
+      }
+    }
+  }
+
+}
+
